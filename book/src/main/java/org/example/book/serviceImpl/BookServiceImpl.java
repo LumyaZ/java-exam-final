f22a4371382090f314aee70ecd322c1ec6adf22e
thomas <cornu.thomas6@gmail.com>
2025-01-21 10:03:25 +0100

    creation userService

---
+++ book/src/main/java/org/example/book/serviceImpl/BookServiceImpl.java
@@ -0,0 +1,36 @@
+package org.example.book.serviceImpl;
+
+import org.example.book.entity.Book;
+import org.example.book.repository.BookRepository;
+import org.example.book.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookServiceImpl implements BookService {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Override
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    @Override
+    public Book getBookById(Long id) {
+        return bookRepository.findById(id).orElseThrow(() -> new RuntimeException("Book not found"));
+    }
+
+    @Override
+    public Book saveBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    @Override
+    public void deleteBook(Long id) {
+        bookRepository.deleteById(id);
+    }
+}
